33a55817fd21462b5c4e57f6aa3e40e447efce83
jerise <38836954+jerise@users.noreply.github.com>
2019-01-12 03:24:15 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,3 @@
-var list = [
-    {'title': 'درس برمجة تطبيقات الأندرويد', 'description': 'سوف تتعلم من خلال هذا المسار طريقة برمجة تطبيقات لأجهزة الآندرويد. تم تطوير نظام تشغيل الآندرويد من قبل شركة Google.', 'url': 'https://beta.barmej.com/tracks/%D8%A8%D8%B1%D9%85%D8%AC%D8%A9-%D8%AA%D8%B7%D8%A8%D9%8A%D9%82%D8%A7%D8%AA-%D8%A7%D9%84%D8%A3%D9%86%D8%AF%D8%B1%D9%88%D9%8A%D8%AF-Android', 'color': '#3d8929'}, 
-    
-    {'title': 'برمجة المواقع مع Django', 'description': 'في هذا المسار ستتعلم برمجة المواقع و الخوادم باستخدام لغة Python ونظام Django لبرمجة تطبيقات المواقع.', 'url': 'https://beta.barmej.com/tracks/%D8%A8%D8%B1%D9%85%D8%AC%D8%A9-%D8%A7%D9%84%D9%85%D9%88%D8%A7%D9%82%D8%B9-%D9%85%D8%B9-%D8%AC%D8%A7%D9%86%D8%BA%D9%88-Django', 'color': '#8803b5'},
-    
-    {'title': 'برمجة تطبيقات الآيفون مع Swift', 'description': 'يشرح هذا المسار طريقة برمجة التطبيقات لأجهزة الآيفون والتي تعمل على نظام iOS الذي تم تطويره من قبل شركة Apple.', 'url': 'https://beta.barmej.com/tracks/%D8%A8%D8%B1%D9%85%D8%AC%D8%A9-%D8%AA%D8%B7%D8%A8%D9%8A%D9%82%D8%A7%D8%AA-%D8%A7%D9%84%D8%A7%D9%8A%D9%81%D9%88%D9%86-%D9%85%D8%B9-%D8%B3%D9%88%D9%8A%D9%81%D8%AA-Swift', 'color': '#d64d48'}, 
 
 function prev(){
         console.log ('البوت الاول');
