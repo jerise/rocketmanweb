42541e39cdd4e42441763f7613d5bb368755f2b6
jerise <38836954+jerise@users.noreply.github.com>
2019-01-14 03:39:24 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,6 @@
-
 function prev(){
         console.log ('البوت الاول');
-        
+       
         document.getElementById("android_title").style.background= "red";
         document.getElementById("android_title").innerHTML="بوتات دسكود حارقة";  
     }
@@ -9,4 +8,4 @@ function prev(){
       console.log ('البوت الثاني');
         document.getElementById("android_title").style.background= "yellow";
         document.getElementById("android_title").innerHTML="بوتات دسكورد ترحيب";  
-    }
+}
